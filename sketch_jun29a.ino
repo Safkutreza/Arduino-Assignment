490434e598aa4270829e1fec858e8917812062cc
K. M. Safkut Reza <rezasafkut@gmail.com>
2025-06-29 11:01:14 +0600

    Add files via upload

---
+++ sketch_jun29a.ino
@@ -0,0 +1,32 @@
+void setup() {
+  pinMode(13,OUTPUT);
+  
+}
+
+
+
+void loop() {
+
+  
+  digitalWrite(13,HIGH);
+  delay(100);
+  digitalWrite(13,LOW);
+  delay(1000);
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);
+  delay(1000);
+  digitalWrite(13,HIGH);
+  delay(2000);
+  digitalWrite(13,LOW);
+  delay(3000);
+  digitalWrite(13,HIGH);
+  delay(500);
+  digitalWrite(13,LOW);
+  delay(1000);
+
+}
+
+
+
+
